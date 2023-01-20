fe703690443da5789f34c8ed3ebfec5aa799236c
Geferson <gefersonjefreey@gmail.com>
2023-01-20 11:25:38 -0300

    fix

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const postgresConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: 'http://ec2-18-231-114-17.sa-east-1.compute.amazonaws.com/',
+    host: '18.231.114.17',
     port: 5432,
     username: 'postgres',
     password: '1234',
