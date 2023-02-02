633278ac7d74fd84aff5926293eeb23919c15fde
Geferson <gefersonjefreey@gmail.com>
2023-02-02 20:26:48 -0300

    feat: add dist of deploy

--- dist/main.js
+++ dist/main.js
@@ -35,6 +35,12 @@ async function bootstrap() {
         credentials: true,
         exposedHeaders: ['API-Token-Expiry'],
     });
+    app.use((req, res, next) => {
+        res.header('Access-Control-Allow-Origin', '*');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        next();
+    });
     const config = new swagger_1.DocumentBuilder()
         .setTitle('API MKS')
         .setDescription('Technical test')
